fe98a85a03a8b3c02bc9075fe4042d6fa2de5afe
Abdul-Qudus Olatunji <fikewabdul@gmail.com>
2025-05-10 00:08:26 -0500

    description change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,7 @@ import '@/styles/tailwind.css'
 export const metadata: Metadata = {
   title: {
     template: '%s - Zekinat Akinwande',
-    default:
-      'Zekinat Akinwande - Product manager, founder, and amateur astronaut',
+    default: 'Zekinat Akinwande - Product manager',
   },
   description:
     'I’m Zekinat, a Product manager and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
